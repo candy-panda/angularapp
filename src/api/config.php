592a168c3ee13000901a51d42e75b87587769c04
Candy Panda <candy_panda@ukr.net>
2016-03-09 19:52:44 +0200

    Update config.php

--- src/api/config.php
+++ src/api/config.php
@@ -3,7 +3,7 @@
  * Database configuration
  */
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'emifol25');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'angularcode');
 
